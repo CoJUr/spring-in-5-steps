029e4e7ecb33170c44811005e1a0a38961bef29c
“Cory <coryurton@gmail.com>
2022-08-13 17:38:27 -0500

    implemented dependency injection - spring coming up next

--- src/main/java/com/mins/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/mins/spring/basics/springin5steps/BinarySearchImpl.java
@@ -4,8 +4,15 @@ public class BinarySearchImpl {
 
     //sort the array, then search it, then return the res index     - understanding tight/loose coupling
 
+    private SortAlgorithm sortAlgorithm;
 
+    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+        super();
+        this.sortAlgorithm = sortAlgorithm;
+    }
 
+    public BinarySearchImpl() {
+    }
 
     public int binarySearch(int[] numbers, int numberToSearchFor) {
 
@@ -13,8 +20,13 @@ public class BinarySearchImpl {
         // not great, may want to change to quick sort or something else sometime. start implementing algorithm outside class
 
         BubbleSortAlgorithm bubbleSort = new BubbleSortAlgorithm();
-        int[] sortedNumbers= bubbleSort.sort(numbers);
+//        int[] sortedNumbers= bubbleSort.sort(numbers);   CANCELED
+//        want to be able to dynamically change the algorithm used to sort the array. How? SortAlgorithm dependency injection
+        int[] sortedNumbers = sortAlgorithm.sort(numbers);
 
+        //have now separated the BinarySearchImpl from the SortAlgorithm.
+
+        System.out.println(sortAlgorithm);
 
 
         //return the index of the number in the array
--- src/main/java/com/mins/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ src/main/java/com/mins/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -8,11 +8,16 @@ public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
 
-		BinarySearchImpl binarySearch = new BinarySearchImpl();
+//		BinarySearchImpl binarySearch = new BinarySearchImpl();
+//		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		//works bc SortAlgorithm is a dependency of BinarySearchImpl
+//		is there a way to avoid needing to create a new instance of SortAlgorithm and BinarySearchImpl?
+
 		int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
 		System.out.println(result);
 
-		SpringApplication.run(SpringIn5StepsApplication.class, args);
+//		SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
 
 
